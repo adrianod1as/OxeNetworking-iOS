f1c7f2b41f0fa3804c84164ec3cb69419258b74d
Adriano Dias <adrianodias@ioasys.com.br>
2020-08-04 21:58:12 -0300

    Add Rx Extensions

---
+++ OxeNetworking/OxeNetworking/OxeNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OxeNetworking : NSObject
+@end
+@implementation PodsDummy_OxeNetworking
+@end
---
+++ OxeNetworking/Sources/RxOxeNetworking/Dispatcher+Rx.swift
@@ -0,0 +1,98 @@
+//
+//  ReplaceMe.swift
+//  OxeNetworking
+//
+//  Created by Adriano Dias on 02/08/20.
+//
+
+import Foundation
+import Moya
+import RxSwift
+import SwiftyJSON
+
+public extension Reactive where Base: Dispatcher {
+
+    func call(endpoint: TargetType) -> Single<Base.DispatcherResponse> {
+        Single.create { [weak base] single in
+            base?.call(endpoint: endpoint) { result in
+                switch result {
+                case let .success(response):
+                    single(.success(response))
+                case let .failure(error):
+                    single(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func getDecodable<T: Decodable>(_ type: T.Type, from endpoint: TargetType) -> Single<T> {
+        Single.create { [weak base] single in
+            base?.getDecodable(type, from: endpoint) { result in
+                switch result {
+                case let .success(response):
+                    single(.success(response))
+                case let .failure(error):
+                    single(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func getJSON(from endpoint: TargetType) -> Single<Any> {
+        Single.create { [weak base] single in
+            base?.getJSON(from: endpoint) { result in
+                switch result {
+                case let .success(response):
+                    single(.success(response))
+                case let .failure(error):
+                    single(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func getSwiftyJSON(from endpoint: TargetType) -> Single<JSON> {
+        Single.create { [weak base] single in
+            base?.getSwiftyJSON(from: endpoint) { result in
+                switch result {
+                case let .success(response):
+                    single(.success(response))
+                case let .failure(error):
+                    single(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func getResponse(from endpoint: TargetType) -> Single<Base.DispatcherResponse>  {
+        Single.create { [weak base] single in
+            base?.getResponse(from: endpoint) { result in
+                switch result {
+                case let .success(response):
+                    single(.success(response))
+                case let .failure(error):
+                    single(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+
+    func performRequest(from endpoint: TargetType) -> Single<Void> {
+        Single.create { [weak base] single in
+            base?.performRequest(from: endpoint) { result in
+                switch result {
+                case let .success(response):
+                    single(.success(response))
+                case let .failure(error):
+                    single(.error(error))
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
